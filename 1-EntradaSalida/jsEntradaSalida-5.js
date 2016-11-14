5ae3e958e0a84e7fdd282c5119ffb175bcdf1773
verobueno <veronica_bueno@yahoo.com.ar>
2016-11-14 19:35:16 -0300

    segundocommit
    ejercicio nro 5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,30 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+	//defino variables nombre y edad
+	var nombre;
+	var edad;
+
+// id="elNombre"
+// id="laEdad"
+
+//var nombre=prompt("ingrese nombre");
+//var edad=prompt("ingrese edad");
+
+nombre=document.getElementById('elNombre').value;
+edad=document.getElementById('laEdad').value;
+
+//document.getElementById('elNombre').value=nombre;
+//document.getElementById('laEdad').value=edad;
+
+alert("Usted se llama: "+nombre+" y su edad es: "+edad);
 }
 
+//
+//declaro las variables linea 6
+//las cargo con getElementById nombre=document.getElementById('elNombre').value;
+//variable del lado izq del = asigna del lado de la derecha lo muestra.
+//donde elNombre es el valor que capturo del html, .value muestra el valor?
+// o las cargo con prompt var nombre=prompt("ingrese nombre");
+//las muestro con Alert
+//o las muestro con document.getElementById('elNombre').value=nombre;
