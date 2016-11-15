dae79402ba2545eece9e8b03d4a63d3ddea50df3
verobueno <veronica_bueno@yahoo.com.ar>
2016-11-15 19:19:19 -0300

    commit1deltp
    Ejercicio FerreteFacturacion

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,59 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+// id="PrecioUno"
+// id="PrecioDos"
+// id="PrecioTres"
+
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	var suma;
+
+	PrecioUno=document.getElementById('PrecioUno').value;
+	PrecioDos=document.getElementById('PrecioDos').value;
+	PrecioTres=document.getElementById('PrecioTres').value;
+
+	suma=parseInt(PrecioUno)+parseInt(PrecioDos)+parseInt(PrecioTres);
+	//Alert("La suma es: "+suma);
+	alert("El precio total es: " + suma);
 	
+
+
 }
 function Promedio () 
 {
-	
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	var promedio;
+
+	PrecioUno=document.getElementById('PrecioUno').value;
+	PrecioDos=document.getElementById('PrecioDos').value;
+	PrecioTres=document.getElementById('PrecioTres').value;
+
+	promedio=(parseInt(PrecioUno)+parseInt(PrecioDos)+parseInt(PrecioTres))/3
+	alert(promedio)
 }
+
 function PrecioFinal () 
 {
+	var PrecioUnoconIva;
+	var PrecioDosconIva;
+	var PrecioTresconIva;
+
+	PrecioUnoconIva=(document.getElementById('PrecioUno').value)*1.21;
+	PrecioDosconIva=(document.getElementById('PrecioDos').value)*1.21;
+	PrecioTresconIva=(document.getElementById('PrecioTres').value)*1.21;
+
+//	alert("El precio de cada producto con IVA es: " + PrecioUnoconIva+ ","+ PrecioDosconIva + ","+PrecioTresconIva);
+
+
+alert ("Precio de los productos: \n "   + PrecioUnoconIva  + "\n"
+									    + PrecioDosconIva  + "\n"
+										+ PrecioTresconIva 		);
 	
 }
+
+//console.log("hola") me muestra el valor de la variable
+//si me paro sobre un texto CRTL D ... Y Selecciona todos los textos que estan en el programa.
