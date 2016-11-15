c3ae9ad41056cc37f473c7c263eaf96db19db9f9
verobueno <veronica_bueno@yahoo.com.ar>
2016-11-15 19:54:29 -0300

    commit5-carpetatp
    carpeta tp

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,56 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+// id="Largo">
+// id="Ancho">
+// id="Radio">
+
+var Largo;
+var Ancho;
+var TotaldeAlambre;
+var Perimetro;
+
+Largo=document.getElementById('Largo').value;
+Ancho=document.getElementById('Ancho').value;
+
+Perimetro=((parseInt(Largo)+parseInt(Ancho))*2);
+TotaldeAlambre=Perimetro*3; //tres vueltas de alambre
+
+alert("La cantidad de alambre necesaria es: " + TotaldeAlambre);
 
 }
 function Circulo () 
 {
 	
+
+//2pi x radio
+
+var Radio;
+var TotaldeAlambre;
+
+
+Radio=document.getElementById('Radio').value;
+
+TotaldeAlambre=(2 * 3.14 * Radio)*3;
+alert("El total de Alambre es: " + TotaldeAlambre);
+
+
+
 }
+
+
 function Materiales () 
 {
 	
+// id="Largo">
+// id="Ancho">
+// id="Radio">
+
+
+
+Largo=document.getElementById('Largo').value;
+Ancho=document.getElementById('Ancho').value;
+
+
+
 }
