acd77065d4082bcba2b9e2aa304c1f38e34c5e34
verobueno <veronica_bueno@yahoo.com.ar>
2016-11-14 22:59:58 -0300

    cuartocommit-ejerc7
    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,86 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+
+	//id="numeroUno"
+	//id="numeroDos"
+	var num1;
+	var num2;
+	//var suma;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById('numeroDos').value;
+
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	//suma=num1+num1
+	// alert("La suma es: "+ (num1+num2));
+	alert("La suma es: "+ (num1+num2));
 }
 
 function restar()
 {
+
+	//id="numeroUno"
+	//id="numeroDos"
+	var num1;
+	var num2;
+	//var resta;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById('numeroDos').value;
+
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	//resta=num1-num2;
+
+	alert("La resta es  "+(num1-num2));
 	
+
 }
 
 function multiplicar()
 { 
 	
+
+	//id="numeroUno"
+	//id="numeroDos"
+	var num1;
+	var num2;
+	var multiplicar;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById('numeroDos').value;
+
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	multiplicar=num1*num2;
+
+	alert("La multiplicacion es: "+multiplicar);
+
 }
 
 function dividir()
 {
+
+	//id="numeroUno"
+	//id="numeroDos"
+	var num1;
+	var num2;
+	var dividir;
+
+	num1=document.getElementById('numeroUno').value;
+	num2=document.getElementById('numeroDos').value;
+
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+
+	dividir=num1/num2;
+
+	alert("La division es: "+dividir);
 	
 }
 
